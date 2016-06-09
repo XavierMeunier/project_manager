0a5002ad312372e968368173ed669246bfee00ee
XavierMeunier <m.xaviermeunier@gmail.com>
2016-06-09 19:28:45 +0200

    feat(validations): Add validations to project & category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+
+  validates :name, presence: true, length: { maximum: 50 }
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+
+  validates :name,        presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :latitude,    presence: true
+  validates :longitude,   presence: true
+  validates :amount,      presence: true
+  validates_numericality_of :amount, greater_than: 0
+
 end
